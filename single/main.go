6920d7dc9642ac94955a74eb1124d09ed8d73f68
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-09-12 15:12:47 +0700

    update build push single

--- single/main.go
+++ single/main.go
@@ -6,26 +6,38 @@ import (
 	"os"
 	"os/exec"
 	"time"
+
+	"github.com/goombaio/namegenerator"
 )
 
 var (
-	// harborURL     = "harbor-s3.poc.workisboring.com"
-	// project       = "knight"
-	// imageName     = "helloword"
-	// imageTag      = "latest"
-	// username      = "robot$loadtest"
-	// password      = "bpyKqEfWFCJuwFrPexqhBTK8RNhZEUyr"
 	harborURL     = os.Getenv("HARBOR_BASE_URL")
 	project       = os.Getenv("HARBOR_PROJECT")
-	imageName     = "helloword"
 	imageTag      = "latest"
-	username      = "robot$loadtest"
-	password      = "bpyKqEfWFCJuwFrPexqhBTK8RNhZEUyr"
+	username      = os.Getenv("HARBOR_USER")
+	password      = os.Getenv("HARBOR_PASS")
 	parallelCount = 5
 	loopCount     = 100 // Number of times each worker will push
 )
 
-func pushImage(workerID int, ch chan time.Duration) {
+func generateImageName() string {
+	seed := time.Now().UTC().UnixNano()
+	nameGenerator := namegenerator.NewNameGenerator(seed)
+	name := nameGenerator.Generate()
+	return name
+}
+
+func buildImage(imageName string) {
+	image := fmt.Sprintf("%s/%s/%s:%s", harborURL, project, imageName, imageTag)
+	cmd := exec.Command("docker", "buildx", "build", "-t", image, ".")
+	building, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Failed to build to image: %v", err)
+	}
+	fmt.Printf("Building: %s\n", (building))
+}
+
+func pushImage(workerID int, ch chan time.Duration, imageName string) {
 	image := fmt.Sprintf("%s/%s/%s:%s", harborURL, project, imageName, imageTag)
 
 	var totalDuration time.Duration
@@ -50,6 +62,12 @@ func pushImage(workerID int, ch chan time.Duration) {
 }
 
 func main() {
+	imageName := generateImageName()
+
+	buildImage(imageName)
+
+	os.Exit(1)
+
 	cmd := exec.Command("docker", "login", harborURL, "-u", username, "-p", password)
 	pushing, err := cmd.CombinedOutput()
 	if err != nil {
@@ -60,7 +78,7 @@ func main() {
 	ch := make(chan time.Duration, parallelCount)
 
 	for i := 0; i < parallelCount; i++ {
-		go pushImage(i, ch)
+		go pushImage(i, ch, imageName)
 	}
 
 	var totalDuration time.Duration
